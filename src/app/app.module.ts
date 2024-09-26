da60ddb22caa4fbe4a27a10ea5217628b85e49b0
omarhaddad2004 <haddadomar739@gmail.com>
2024-09-26 18:44:36 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+bootstrapApplication(AppComponent, );
